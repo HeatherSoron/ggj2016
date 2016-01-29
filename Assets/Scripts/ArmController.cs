a3793732e771d67a7fd62132a691439ca70d829c
Bryce <brycebooth@hotmail.com>
2016-01-30 12:06:11 +1300

    CD

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ArmController : MonoBehaviour
+{
+
+    public enum ESide
+    {
+        Left,
+        Right
+    }
+
+    public enum EDirection
+    {
+        Down,
+        Forward,
+        HalfOutward,
+        Outward
+    }
+
+    public EDirection Direction
+    {
+        get { return m_direction; }
+        set
+        {
+            m_direction = value;
+
+            UpdateTargetRotation();
+        }
+    }
+
+    void Start()
+    {
+        UpdateTargetRotation();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Direction = EDirection.Forward;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Direction = EDirection.HalfOutward;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Direction = EDirection.Outward;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Direction = EDirection.Down;
+        }
+
+        UpdateRotation();
+    }
+
+    void UpdateRotation()
+    {
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation  , m_targetRotation, m_velocity * Time.deltaTime);
+    }
+
+    void UpdateTargetRotation()
+    {
+        switch (m_direction)
+        {
+            case EDirection.Down:
+                m_targetRotation = Quaternion.identity;
+                break;
+
+            case EDirection.Forward:
+                m_targetRotation.eulerAngles = new Vector3(0.0f, 0.0f, 90.0f);
+                break;
+
+            case EDirection.HalfOutward:
+                m_targetRotation.eulerAngles = m_side == ESide.Left ? new Vector3(270.0f, 45.0f, 00.0f) : new Vector3(90.0f, 315.0f, 00.0f);
+                break;
+
+            case EDirection.Outward:
+                m_targetRotation.eulerAngles = m_side == ESide.Left ? new Vector3(270.0f, 0.0f, 00.0f) : new Vector3(90.0f, 0.0f, 00.0f);
+                break;
+        }
+    }
+
+    [SerializeField]
+    ESide m_side = ESide.Left;
+
+    [SerializeField]
+    float m_velocity = 90.0f;
+
+    EDirection m_direction = EDirection.Down;
+
+    Quaternion m_targetRotation = Quaternion.identity;
+
+}
---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HeadController : MonoBehaviour
+{
+
+    public enum EDirection
+    {
+        Forward,
+        Left,
+        Right,
+        Up
+    }
+
+    public EDirection Direction
+    {
+        get { return m_direction; }
+        set
+        {
+            m_direction = value;
+
+            UpdateTargetRotation();
+        }
+    }
+
+    void Start()
+    {
+        UpdateTargetRotation();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Direction = EDirection.Up;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Direction = EDirection.Left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Direction = EDirection.Right;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Direction = EDirection.Forward;
+        }
+
+        UpdateRotation();
+    }
+
+    void UpdateRotation()
+    {
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation  , m_targetRotation, m_velocity * Time.deltaTime);
+    }
+
+    void UpdateTargetRotation()
+    {
+        switch (m_direction)
+        {
+            case EDirection.Forward:
+                m_targetRotation = Quaternion.identity;
+                break;
+
+            case EDirection.Left:
+                m_targetRotation.eulerAngles = new Vector3(0.0f, -30.0f, 0.0f);
+                break;
+
+            case EDirection.Right:
+                m_targetRotation.eulerAngles = new Vector3(0.0f, 30.0f, 0.0f);
+                break;
+
+            case EDirection.Up:
+                m_targetRotation.eulerAngles = new Vector3(0.0f, 0.0f, 30.0f);
+                break;
+        }
+    }
+
+    [SerializeField]
+    float m_velocity = 360.0f;
+
+    EDirection m_direction      = EDirection.Forward;
+    Quaternion m_targetRotation = Quaternion.identity;
+
+}
