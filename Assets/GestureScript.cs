340d4603b0057c6fba818cd6cb30f72e221e5a94
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2016-01-30 16:30:15 +1300

    Bunch of tweaks, including hooking up the UI (kinda)

---
+++ Assets/GestureScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GestureScript : MonoBehaviour {
+	public int Player = 1;
+
+	public float minTimeDiff = 0.2f;
+	private float lastGestureTime = 0;
+
+	private int currentGesture = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GestureStart("" + DemonScript.gestures [currentGesture])) {
+			currentGesture++;
+		}
+		if (currentGesture >= DemonScript.gestures.Length) {
+			DemonScript.power += 0.1f;
+			currentGesture = 0;
+		}
+	}
+
+	public bool GestureStart (string gesture) {
+		bool hasGesture = GestureAllowed() && Input.GetButtonDown ("P" + Player + "_" + gesture);
+		if (hasGesture) {
+			lastGestureTime = Time.time;
+		}
+		return hasGesture;
+	}
+
+	bool GestureAllowed() {
+		// either the last gesture must have been a while ago, OR it must have been this tick
+		return lastGestureTime + minTimeDiff < Time.time || lastGestureTime == Time.time;
+	}
+}
--- Assets/Scripts/DemonScript.cs
+++ Assets/Scripts/DemonScript.cs
@@ -8,41 +8,99 @@ public class DemonScript : MonoBehaviour {
 	public GameObject Icon2;
 	public GameObject Icon3;
 
-	public int failCount = 0;
-	public int winCount = 0;
-	public double power = 0;
+	public enum EGesture {
+		Wide,
+		Middle,
+		Forward,
+		Down
+	};
 
-	public int button = 1;
-	public float lastAttack;
+	public static EGesture[] gestures = new EGesture[3];
+
+	public static float power = 0.5f;
+	public float powerFade = 0.02f;
+
+
+	public GameObject powerBar;
+
+	public float changeDelay = 4f;
+	private float lastChange = 0;
 
 	// Use this for initialization
 	void Start () {
-		lastAttack = Time.time;
+		lastChange = Time.time;
+		gestures [0] = EGesture.Wide;
+		gestures [1] = EGesture.Forward;
+		gestures [2] = EGesture.Middle;
+
+		ShowGesture (Icon1, gestures [0]);
+		ShowGesture (Icon2, gestures [1]);
+		ShowGesture (Icon3, gestures [2]);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetButtonDown("Fire" + button)) {
-			power += 1;
-			button %= 3;
-			button++;
+		if (lastChange + changeDelay < Time.time) {
+			for (int i = 0; i < gestures.Length; ++i) {
+				gestures [i] = RandomGesture ();
+			}
+			ShowGesture (Icon1, gestures [0]);
+			ShowGesture (Icon2, gestures [1]);
+			ShowGesture (Icon3, gestures [2]);
+			lastChange = Time.time;
 		}
 
+		power -= powerFade * Time.deltaTime;
 
-		if (Time.time > lastAttack + 5) {
-			if (power < 10) {
-				failCount += 1;
-			} else {
-				winCount += 1;
-			}
+		powerBar.GetComponent<Slider> ().value = power;
+	}
 
-			power = 0;
-			lastAttack = Time.time;
+	void ShowGesture(GameObject icon, EGesture gesture) {
+		icon.GetComponent<Image>().color = GetGestureColor(gesture);
+		icon.GetComponentInChildren<Text> ().text = GetGestureText (gesture);
+	}
+
+	Color32 GetGestureColor(EGesture gesture) {
+		switch (gesture) {
+		case EGesture.Forward:
+			return new Color32 (0, 255, 33, 255);
+			break;
+		case EGesture.Wide:
+			return new Color32 (255, 33, 0, 255);
+			break;
+		case EGesture.Middle:
+			return new Color32 (0, 0, 255, 255);
+			break;
+		case EGesture.Down:
+			return new Color32 (200, 160, 0, 255);
+			break;
+		default:
+			return new Color32 (0, 0, 0, 255);
+			break;
 		}
 	}
 
-	void ShowGesture(GameObject icon, Color32 color) {
-		icon.GetComponent<Image>().color = color;
+	string GetGestureText(EGesture gesture) {
+		switch (gesture) {
+		case EGesture.Forward:
+			return "A";
+			break;
+		case EGesture.Wide:
+			return "B";
+			break;
+		case EGesture.Middle:
+			return "X";
+			break;
+		case EGesture.Down:
+			return "Y";
+			break;
+		default:
+			return "?";
+			break;
+		}
+	}
+
+	EGesture RandomGesture() {
+		return (EGesture)Random.Range (0, System.Enum.GetValues (typeof(EGesture)).Length);
 	}
 }
